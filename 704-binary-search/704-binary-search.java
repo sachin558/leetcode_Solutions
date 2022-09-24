6da29dc99106dd3351b530abda6ac511e2f43520
Sachin kumar <81067204+sachin558@users.noreply.github.com>
2022-09-24 22:51:27 +0530

    Time: 0 ms (100.00%), Space: 43.1 MB (90.06%) - LeetHub

--- 704-binary-search/704-binary-search.java
+++ 704-binary-search/704-binary-search.java
@@ -3,15 +3,15 @@ class Solution {
       int left=0,right=nums.length-1,mid=0;
         while(left<=right){
             mid=left+(right-left)/2;
-            if(nums[mid]==target) {
+            if(nums[mid]==target) 
                 return mid;
-            }
-            if(nums[mid]<target){
+            
+            if(nums[mid]<target)
                 left=mid+1;
-            }
-            else{
+            
+            else
                 right=mid-1;
-            }
+            
         }
         return -1;
     }
