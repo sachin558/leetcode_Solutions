fca5cbee33970ed5f2784590679a4d11a53d97e7
Sachin kumar <81067204+sachin558@users.noreply.github.com>
2023-01-03 16:38:55 +0530

    Time: 1 ms (73.58%), Space: 43.3 MB (48.17%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,21 +1,15 @@
 public class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-
-        List<List<Integer>> res = new ArrayList<>();
-        if(nums == null || nums.length == 0) return res;
-        List<Integer> cur = new ArrayList<>();
-        Arrays.sort(nums);
-        dfs(nums, res, 0, cur);
-        return res;
-    }
-
-    private void dfs(int[] nums, List<List<Integer>> res, int start, List<Integer> cur){
-        res.add(new ArrayList<Integer>(cur));
-        for(int i=start; i< nums.length; i++){
-            cur.add(nums[i]);
-
-            dfs(nums, res, i+1, cur); 
-            cur.remove(cur.size()-1);
+        List<List<Integer>> ans=new ArrayList<>();
+        backtrack(nums,0,new ArrayList<Integer>(),ans);
+        return ans;
+        }
+    public void backtrack(int []nums,int strat_ind,List<Integer> trac,List<List<Integer>> ans ){
+        ans.add(new ArrayList<>(trac));
+        for(int i=strat_ind;i<nums.length;i++){
+            trac.add(nums[i]);
+            backtrack(nums,i+1,trac,ans);
+            trac.remove(trac.size()-1);
         }
     }
-}
+    }
