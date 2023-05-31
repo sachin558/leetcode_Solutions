fa03ed7fdd1ae14fe9da52b125c187dd59b29ad1
Sachin kumar <81067204+sachin558@users.noreply.github.com>
2023-05-31 16:28:55 +0530

    Time: 1 ms (83.62%), Space: 41.7 MB (64.33%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -1,17 +1,14 @@
 class Solution {
     public String gcdOfStrings(String str1, String str2) {
-     if (!(str1 + str2).equals(str2 + str1)) {
-            return "";
+     if(!(str1+str2).equals(str2+str1)){
+         return "";
+     }
+        int l1=str1.length();
+        int l2=str2.length();
+        while(l1!=l2){
+            if(l1>l2) l1-=l2;
+            else l2-=l1;
         }
-        
-        // Count gcd of string lengths
-        int l1 = str1.length();
-        int l2 = str2.length();
-        while (l1 != l2) {
-            if (l1 > l2) l1 -= l2;
-            else l2 -= l1;
-        }
-
-        return str1.substring(0, l1);
+        return str1.substring(0,l1);
     }
 }
