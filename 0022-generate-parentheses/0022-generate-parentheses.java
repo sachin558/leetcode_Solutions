7f56d255c3cf1370f07c3edb160734076139012f
Sachin kumar <81067204+sachin558@users.noreply.github.com>
2023-06-06 07:28:21 +0530

    Time: 1 ms (94.32%), Space: 43.4 MB (26.30%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,19 +1,19 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-       List<String> ans = new ArrayList();
+      List<String> ans=new ArrayList<>();
         back("",0,0,ans,n);
-
         return ans;
     }
-
-    void back(String current,int op , int cl , List<String> ans, int n){
-        if(current.length()==2*n){
-            ans.add(current);
-            return;
+    public void back(String curr,int op, int cl , List<String > ans, int n){
+        if(curr.length() ==2*n){
+            ans.add(curr);
+            return ;
         }
-        if(op<n)
-        back(current+"(", op+1,cl,ans,n);
-        if(cl<op)
-        back(current+")",op,cl+1,ans,n);
+        if(op<n){
+            back(curr+"(",op+1,cl,ans,n);
+        }
+        if(cl<op){
+            back(curr+")",op,cl+1,ans,n);
+        }
     }
 }
